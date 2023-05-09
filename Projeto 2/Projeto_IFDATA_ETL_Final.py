a1f58168d125a0923d26c6fb763f9e1f52412fcb
lcbarros <lucasbrrsilva@gmail.com>
2023-05-09 20:48:30 -0300

    Otimização do processo de ETL

---
+++ Projeto 2/Projeto_IFDATA_ETL_Final.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from Projeto_IFDATA_Functions import Extract, Transform, Load
+
+#customiza lista de relatorios a serem extraidos
+#loop a cada item da lista
+    #captura dados PF
+    #transforma dados PF
+    #salva dados PF
+
+    #captura dados PJ
+    #transforma dados PJ
+    #salva dados PJ
+
+    #captura dados codint
+    #transforma dados codint
+    #salva dados codint
+
+    #Proximo loop
+
+#cria df para acompanhar sucesso da lista
